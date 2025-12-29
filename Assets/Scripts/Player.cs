64783d2c3529cf4318d5b6c83a61d4a32e1d717a
FranzLor <FranzLor64@yahoo.com>
2025-12-29 16:36:30 -0700

    Player Script
    player input horizontal input and sprite flipping in script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-    void Start() {
+public class Player : MonoBehaviour {
 
+    Transform spriteComponent;
+    float flipX;
+
+    void Start() {
+        spriteComponent = GetComponentInChildren<SpriteRenderer>().transform;
+        flipX = spriteComponent.localScale.x;
     }
 
     void Update() {
-        
+        // grid-like movement input
+        float horizontal = System.Math.Sign(Input.GetAxisRaw("Horizontal"));
+
+        // flips sprite based on movement direction
+        if (Mathf.Abs(horizontal) > 0) { 
+            spriteComponent.localScale = new Vector2(flipX * horizontal, spriteComponent.localScale.y);
+        }
     }
+
 }
